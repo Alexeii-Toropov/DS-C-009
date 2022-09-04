9f6b00494b4ce568c0f4d7d810dcefc45fa23ce5
Alexeii Toropov <leha.toropov.04@mail.ru>
2022-09-04 14:49:14 +0300

    fin

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("введие число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("введие число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int count = N - M;
+for (int i = 0; i <= count; i++)
+{
+    if (M > 0)
+    {
+        Console.Write(M + " ");
+    }
+    M++;
+}
