e36ab74f7865d8237f09f95a20ebe0c4f6f18801
Алексей <110294020+Alexeii-Toropov@users.noreply.github.com>
2022-09-04 14:57:09 +0300

    Add files via upload

---
+++ C#009.1/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.WriteLine("введие число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("введие число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int count = N - M;
+int res = 0;
+for (int i = 0; i <= count; i++)
+{
+    if (M > 0)
+    {
+        res += M;
+    }
+    M++;
+}
+System.Console.WriteLine(res);
